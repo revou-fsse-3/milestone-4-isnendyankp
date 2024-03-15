e24871e1f90bed1ba59886e942e6862e4b444e5d
isnendyankp <isnendyankp@gmail.com>
2024-03-15 19:19:53 +0700

    Add JWT authentication to the app

--- Banking_app/app/__init__.py
+++ Banking_app/app/__init__.py
@@ -3,11 +3,16 @@ from dotenv import load_dotenv
 from app.controllers.user_management_route import user_routes
 from app.controllers.account_management_route import account_routes
 from app.controllers.transaction_management_route import transaction_routes
-
+from flask_jwt_extended import JWTManager
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
+
+# Set the JWT secret key
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
 
 #register blueprints
 app.register_blueprint(user_routes)
