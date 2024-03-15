b40695a1acc9de882f8038a433962a659f0e2699
isnendyankp <isnendyankp@gmail.com>
2024-03-15 19:18:36 +0700

    Add transaction management route

--- Banking_app/app/__init__.py
+++ Banking_app/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from dotenv import load_dotenv
 from app.controllers.user_management_route import user_routes
 from app.controllers.account_management_route import account_routes
+from app.controllers.transaction_management_route import transaction_routes
 
 
 load_dotenv()
@@ -11,6 +12,7 @@ app = Flask(__name__)
 #register blueprints
 app.register_blueprint(user_routes)
 app.register_blueprint(account_routes)
+app.register_blueprint(transaction_routes)
 
 @app.route('/')
 def index():
