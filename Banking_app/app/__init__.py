c213e48c79a74b15a3ab4479653da772c7a44980
isnendyankp <isnendyankp@gmail.com>
2024-03-15 18:33:53 +0700

    Add account management routes

--- Banking_app/app/__init__.py
+++ Banking_app/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from dotenv import load_dotenv
 from app.controllers.user_management_route import user_routes
+from app.controllers.account_management_route import account_routes
 
 
 load_dotenv()
@@ -9,6 +10,7 @@ app = Flask(__name__)
 
 #register blueprints
 app.register_blueprint(user_routes)
+app.register_blueprint(account_routes)
 
 @app.route('/')
 def index():
